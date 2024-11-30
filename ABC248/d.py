5e96ffa6913dc2bcbe25cc1e122132d9814e0a11
sthr630-arcturus <s_izumi630y@yahoo.co.jp>
2024-11-30 10:35:53 +0900

    ABC248 level_d passed test case

--- ABC248/d.py
+++ ABC248/d.py
@@ -1,28 +1,3 @@
-"""
-n = int(input())
-a = list(map(int, input().split()))
-
-q = int(input())
-for _ in range(q):
-    l, r, x = map(int, input().split())
-    l -= 1
-    r -= 1
-    s = [0]*n
-
-    if a[0] == x:
-        s[0] = 1
-
-    for i in range(1, n):
-        if a[i] == x:
-            s[i] = s[i-1] + 1
-        else:
-            s[i] = s[i-1]
-
-    if l == 0:
-        print(s[r])
-    else:
-        print(s[r] - s[l])
-"""
 def lower_idx(a, x):
     l = -1
     r = len(a)
@@ -35,7 +10,7 @@ def lower_idx(a, x):
     return r
 
 def upper_idx(a, x):
-    l = 0
+    l = -1
     r = len(a)
     while l+1 < r:
         mid = (l+r) //2
@@ -52,14 +27,13 @@ idx = [[] for _ in range(n+1) ]
 for i in range(n):
     idx[a[i]].append(i+1)
 
-print(idx)
 q = int(input())
 for i in range(q):
     l, r, x = map(int, input().split())
-    print(lower_idx(idx[x], r))
-    print(upper_idx(idx[x], l))
-    print(lower_idx(idx[x], r) - upper_idx(idx[x], l))
-    print("")
+    #print(f"l:{l}  {lower_idx(idx[x], l)}")
+    #print(f"r:{r}  {upper_idx(idx[x], r)}")
+    print(upper_idx(idx[x], r) - lower_idx(idx[x], l)+1)
+    #print("")
 
 
 
