edb9543c9e701cb26fb5f02a01d90df0b4ea49a5
sthr630-sirius <100351188+sthr630-sirius@users.noreply.github.com>
2025-11-18 22:34:21 +0900

    ABC303 level_D passed test case

--- ABC303/d.py
+++ ABC303/d.py
@@ -9,9 +9,9 @@ dp_caps_on = [inf]*(n+1)
 for i in range(0, n):
     if s[i] == "a":
         dp_caps_off[i+1] = min(dp_caps_off[i]+x, dp_caps_on[i]+z+x)
-        dp_caps_on[i+1] = dp_caps_on[i]+y
+        dp_caps_on[i+1] = min(dp_caps_off[i]+z+y, dp_caps_on[i]+y)
     elif s[i] == "A":
-        dp_caps_off[i+1] = dp_caps_off[i]+y
+        dp_caps_off[i+1] = min(dp_caps_off[i]+y, dp_caps_on[i]+z+y)
         dp_caps_on[i+1] = min(dp_caps_on[i]+x, dp_caps_off[i]+z+x)
 
 print(min(dp_caps_off[n], dp_caps_on[n]))
