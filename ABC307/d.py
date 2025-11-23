d371aca42441920df24dc630dadc3150347ecaa0
sthr630-sirius <100351188+sthr630-sirius@users.noreply.github.com>
2025-11-23 22:55:15 +0900

    ABC307 level_d passed test case

--- ABC307/d.py
+++ ABC307/d.py
@@ -1,12 +1,31 @@
 n = int(input())
-s = input()
-left_angle_idx = [n+1]
+s = list(input())
+ans = ""
+
+left_angle_idx = [n]
+del_sect = []
+
 for i in range(n):
     if s[i] == "(":
         left_angle_idx.append(i)
     elif s[i] == ")":
         s_idx = left_angle_idx.pop()
-        e_idx = i+1
-        print(s[s_idx])
-        print(s[e_idx])
-print(s)
+        if s_idx == n:
+            left_angle_idx.append(n)
+        else:
+            e_idx = i+1
+            del_sect.append([s_idx, e_idx])
+
+del_sect.sort()
+
+for s_idx, e_idx in del_sect:
+    if s[s_idx] == "-":
+        continue
+    for i in range(s_idx, e_idx):
+        s[i] = "-"
+
+for i in range(n):
+    if s[i] != "-":
+        ans += s[i]
+
+print(ans)
