42d2bb75714a7ece16f8d522ab45de34e181b0cf
sthr630-sirius <100351188+sthr630-sirius@users.noreply.github.com>
2025-11-28 23:07:58 +0900

    ABC308 level_c passed test case

--- ABC308/c.py
+++ ABC308/c.py
@@ -1,47 +1,25 @@
-def gcd(x, y):
-    r = x % y
-    if r == 0:
-        return y
-
-    return  gcd(y, r)
+from functools import cmp_to_key
 
 n = int(input())
 player_info = []
 
 for i in range(n):
     a, b = map(int, input().split())
-    p = a / (a+b)
-    if a != 0 and b !=0:
-        g = gcd(a, b)
-        player_info.append([p, i+1, a//g, b//g])
+    player_info.append([a,  a+b, i+1])
+
+#player_info.sort(key=lambda x: -x[2])
+
+def cmp(a, b):
+    p = a[0]*b[1]
+    q = a[1]*b[0]
+    if p<q:
+        return 1
+    elif p>q:
+        return -1
     else:
-        player_info.append([p, i+1, a, b])
+        return 0
 
-#print(player_info)
-player_info.sort(reverse=True)
-#print(player_info)
-s_idx = 0
-
-while s_idx < n-1:
-    e_idx = s_idx + 1
-    cnt = 0
-    while e_idx < n:
-        if player_info[s_idx][0] == player_info[e_idx][0]:
-            cnt += 1
-            e_idx += 1
-        else:
-            break
-
-    for i in range(cnt//2+1):
-        #print("---")
-        #print(cnt)
-        #print("---")
-        player_info[s_idx+i], player_info[s_idx+cnt-i] = player_info[s_idx+cnt-i], player_info[s_idx+i]
-
-    s_idx = e_idx
+player_info.sort(key=cmp_to_key(cmp))
 
 for i in range(n):
-    print(player_info[i][1], end=" ")
-
-#print("")
-#print(player_info)
+    print(player_info[i][2], end=" ")
