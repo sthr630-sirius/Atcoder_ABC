1aa31f3fd1f1e64c864dd62d48af7c68c0b58b46
sthr630-arcturus <s_izumi630y@yahoo.co.jp>
2025-12-14 20:33:58 +0900

    ABC313 level_c passed test case

--- ABC312/c.cpp
+++ ABC312/c.cpp
@@ -25,24 +25,13 @@ int main(){
     sort(a.begin(), a.end());
     sort(b.rbegin(), b.rend());
     
-    cout << "----" << endl;
-    for(auto aa:a) cout << aa << " ";
-    cout << endl;
-    for(auto bb:b) cout << bb << " ";
-    cout << endl;
-    cout << "----" << endl;
-    
     int buyer, seller;
     int ans;
-    //ans = b.back()+1;
-
-    vector<int> sell(n);
     vector<int> price(n);
 
     for(int i=0; i<n; i++){
         buyer = i+1;
         seller = binary_search(a[i], b);
-        sell[i] = seller;
         
         if(seller == 0){
             price[i] = b[0]+1;
@@ -52,19 +41,6 @@ int main(){
             price[i] = b[i+1]+1;
         } 
     }
-
-    for(int i=0; i<n; i++){
-        cout << i+1 << " ";
-    }
-    cout << endl;
-    for(int i=0; i<n; i++){
-        cout << sell[i] << " ";
-    }
-    cout << endl;
-    for(int i=0; i<n; i++){
-        cout << price[i] << " ";
-    }
-    cout << endl;
 
     ans = *min_element(begin(price), end(price));
     cout << ans << endl;
---
+++ ABC313/c.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    vector<long long> b(n);
+    long long all_sum = 0;
+    long long ans = 0;
+    all_sum = 0;
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+        all_sum += a[i];
+    }
+
+    for(int i=0; i<n; i++) b[i] = all_sum / n;
+    for(int i=0; i<all_sum%n; i++) b[i]++;
+
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+
+    for(int i=0; i<n; i++) ans += abs(a[i]-b[i]);
+    ans = ans / 2;
+
+    for(auto p:a) cout << p << " ";
+    cout << endl;
+
+    for(auto p:b) cout << p << " ";
+    cout << endl;
+
+    cout << ans << endl;
+
+    return 0;
+}
