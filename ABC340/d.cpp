97dada50e09e516594c7060db839a5ae77112700
sthr630-sirius <100351188+sthr630-sirius@users.noreply.github.com>
2025-12-27 11:23:06 +0900

    ABC340 tmp

--- ABC340/d.cpp
+++ ABC340/d.cpp
@@ -6,6 +6,7 @@ using namespace std;
 void bfs(int n, vector<vector<pair<int, int>>>& g){
     //vector<bool> is_visited(n, false);
     long long INF = 1000000000000000;
+    vector<bool> is_visited(n, false);
     vector<long long> time(n, INF);
     int now_v, next_v;
     int child_v, child_t;
@@ -14,27 +15,50 @@ void bfs(int n, vector<vector<pair<int, int>>>& g){
 
     now_v = 0;
     time[now_v] = 0;
+    is_visited[now_v] = true;
 
-    cout << "now_v: " << now_v << endl;
+    for(auto t:time) cout << t << " ";
+    cout << endl;
+    for(auto b:is_visited) cout << b << " ";
+    cout << endl;
+
+    //cout << "now_v: " << now_v << endl;
     while(now_v != n-1){
         min_time = INF;
         for(auto child_info : g[now_v]){
             child_v = child_info.first;
             child_t = child_info.second;
+            if(is_visited[child_v]) continue;
+            //cout << "child_v: " << child_v << endl;
             time[child_v] = min(time[child_v], time[now_v]+child_t);
 
+            //cout << "time: " << time[child_v] << endl;
+
             if(time[child_v] <= min_time){
+                //cout << "if----" << endl;
+                //cout << "child_v: " << child_v << "  --> next_v" << endl; 
+                
                 min_time = time[child_v];
                 next_v = child_v;
+                //cout << "next_v: " << next_v << endl;
             }
         }
+        is_visited[next_v] = true;
+        //cout << "loop out for: " << endl;
+        cout << endl;
         now_v = next_v;    
+        cout << "now_v: " << now_v << endl;
+        for(auto t:time) cout << t << " ";
+        cout << endl;
+        for(auto b:is_visited) cout << b << " ";
+        cout << endl;
     }
-        
+    
+    /*
     cout << "-------debug-----------" << endl;
     for(auto t:time) cout << t << " ";
     cout << endl;
-    
+    */
     cout << time[n-1] << endl;
 
 }
