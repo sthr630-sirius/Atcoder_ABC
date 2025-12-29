b0855f954dbef8e0e4f1b6c6c67273816f2a36db
sthr630-arcturus <s_izumi630y@yahoo.co.jp>
2025-12-29 21:33:37 +0900

    ABC340 level_d passed test case

--- ABC340/d.cpp
+++ ABC340/d.cpp
@@ -3,73 +3,19 @@
 #include<queue>
 #include<algorithm>
 using namespace std;
-void bfs(int n, vector<vector<pair<int, int>>>& g){
-    //vector<bool> is_visited(n, false);
-    long long INF = 1000000000000000;
-    vector<bool> is_visited(n, false);
-    vector<long long> time(n, INF);
-    int now_v, next_v;
-    int child_v, child_t;
-    vector<pair<int, int>> child_info_list; 
-    long long min_time = INF;
-
-    now_v = 0;
-    time[now_v] = 0;
-    is_visited[now_v] = true;
-
-    for(auto t:time) cout << t << " ";
-    cout << endl;
-    for(auto b:is_visited) cout << b << " ";
-    cout << endl;
-
-    //cout << "now_v: " << now_v << endl;
-    while(now_v != n-1){
-        min_time = INF;
-        for(auto child_info : g[now_v]){
-            child_v = child_info.first;
-            child_t = child_info.second;
-            if(is_visited[child_v]) continue;
-            //cout << "child_v: " << child_v << endl;
-            time[child_v] = min(time[child_v], time[now_v]+child_t);
-
-            //cout << "time: " << time[child_v] << endl;
-
-            if(time[child_v] <= min_time){
-                //cout << "if----" << endl;
-                //cout << "child_v: " << child_v << "  --> next_v" << endl; 
-                
-                min_time = time[child_v];
-                next_v = child_v;
-                //cout << "next_v: " << next_v << endl;
-            }
-        }
-        is_visited[next_v] = true;
-        //cout << "loop out for: " << endl;
-        cout << endl;
-        now_v = next_v;    
-        cout << "now_v: " << now_v << endl;
-        for(auto t:time) cout << t << " ";
-        cout << endl;
-        for(auto b:is_visited) cout << b << " ";
-        cout << endl;
-    }
-    
-    /*
-    cout << "-------debug-----------" << endl;
-    for(auto t:time) cout << t << " ";
-    cout << endl;
-    */
-    cout << time[n-1] << endl;
-
-}
 
 int main(){
     int n;
     cin >> n;
-    vector<vector<pair<int, int>>> g(n);
-    vector<int> parent(n);
-    vector<int> in_edge(n);
     int a, b, x;
+    long long INF = 1000000000000000;
+    vector<vector<pair<int, int>>> g(n);
+    vector<long long> time(n, INF);
+    vector<bool> is_visited(n, false);
+    priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> next_v_que;
+    pair<long long, int> now_v_info;
+    int now_v;
+    long long now_t;
 
     for(int i=0; i<n-1; i++){
         cin >> a >> b >> x;
@@ -77,8 +23,32 @@ int main(){
         g[i].push_back({i+1, a});
         g[i].push_back({x, b});
     }
+    
+    time[0] = 0;
+    next_v_que.push({time[0], 0});
 
-    bfs(n, g);
+    int child_v, child_w;
+
+    while(!next_v_que.empty()){
+        now_v_info = next_v_que.top();
+        next_v_que.pop();
+        now_t = now_v_info.first;
+        now_v = now_v_info.second;
+        if(is_visited[now_v]) continue;
+        is_visited[now_v] = true;
+
+        //for(auto child_info:g[now_v]){
+        for(auto [child_v, child_w]:g[now_v]){
+            //child_v = child_info.first;
+            //child_w = child_info.second;
+            
+            if(is_visited[child_v]) continue;
+            time[child_v] = min(time[child_v], time[now_v]+child_w);
+            next_v_que.push({time[child_v], child_v});
+        }
+    }
+
+    cout << time[n-1] << endl;
 
     return 0;
 }
