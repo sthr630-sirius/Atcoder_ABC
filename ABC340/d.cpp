0b7b47a1dc9ea7e34fedf455cda8143826ca364e
sthr630-arcturus <s_izumi630y@yahoo.co.jp>
2026-01-01 08:49:00 +0900

    ABC362 level_d passed test case

--- ABC340/d.cpp
+++ ABC340/d.cpp
@@ -13,9 +13,6 @@ int main(){
     vector<long long> time(n, INF);
     vector<bool> is_visited(n, false);
     priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> next_v_que;
-    pair<long long, int> now_v_info;
-    int now_v;
-    long long now_t;
 
     for(int i=0; i<n-1; i++){
         cin >> a >> b >> x;
@@ -27,21 +24,13 @@ int main(){
     time[0] = 0;
     next_v_que.push({time[0], 0});
 
-    int child_v, child_w;
-
     while(!next_v_que.empty()){
-        now_v_info = next_v_que.top();
+        auto [now_t, now_v] = next_v_que.top();
         next_v_que.pop();
-        now_t = now_v_info.first;
-        now_v = now_v_info.second;
         if(is_visited[now_v]) continue;
         is_visited[now_v] = true;
 
-        //for(auto child_info:g[now_v]){
-        for(auto [child_v, child_w]:g[now_v]){
-            //child_v = child_info.first;
-            //child_w = child_info.second;
-            
+        for(auto [child_v, child_w]:g[now_v]){            
             if(is_visited[child_v]) continue;
             time[child_v] = min(time[child_v], time[now_v]+child_w);
             next_v_que.push({time[child_v], child_v});
---
+++ ABC362/d.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+int main(){
+    int n, m;
+    cin >> n >> m;
+    vector<int> v_weight(n);
+    vector<vector<pair<int, int>>> g(n);
+    int u, v, b;
+
+    for(int i=0; i<n; i++) cin >> v_weight[i];
+    for(int i=0; i<m; i++){
+        cin >> u >> v >> b;
+        u--;
+        v--;
+        g[u].push_back({v, b});
+        g[v].push_back({u, b});
+    }
+
+    long long INF = 9000000000000000000;
+    vector<long long> sum_weight(n, INF);
+    vector<bool> is_visited(n, false);
+    priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> next_v_que;
+    
+    sum_weight[0] = v_weight[0];
+    next_v_que.push({sum_weight[0], 0});
+
+    while(!next_v_que.empty()){
+        int now_v = next_v_que.top().second;
+        next_v_que.pop();
+        
+        if(is_visited[now_v]) continue;
+        is_visited[now_v] = true;
+        //cout << "now_v: " << now_v+1 << endl;
+        
+        for(auto [next_v, e_weight]:g[now_v]){
+            if(is_visited[next_v]) continue;
+            sum_weight[next_v] = min(sum_weight[next_v], sum_weight[now_v] + e_weight + v_weight[next_v]);
+            next_v_que.push({sum_weight[next_v], next_v});
+        }
+
+        //for(auto w:sum_weight) cout << w << " ";
+        //cout << endl;
+    }
+
+    for(int i=1; i<n; i++) cout << sum_weight[i] << " ";
+    cout << endl;
+}
