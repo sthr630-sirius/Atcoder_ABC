09312e501bef71ddce17479e8aa180242dad37a9
sthr630-sirius <100351188+sthr630-sirius@users.noreply.github.com>
2026-08-23 23:07:20 +0900

    ABC448 level_d passed test case

--- ABC448/d.cpp
+++ ABC448/d.cpp
@@ -1,27 +1,38 @@
 #include<iostream>
 #include<vector>
+#include<set>
 #include<algorithm>
 using namespace std;
-int dfs(int now_v, vector<vector<int>> &g, vector<bool> &is_visited, vector<int> &path){
+int dfs(int now_v, vector<vector<int>> &g, vector<bool> &is_visited, vector<int> &number, vector<int> &path, set<int> &path_number, bool is_duplicated, vector<string> &ans){
+    
     is_visited[now_v] = true;
     path.push_back(now_v);
 
-    cout << "--path--" << endl;
-    for(auto v:path) cout << v+1 << ", ";
-    cout << endl;
+    if(is_duplicated){
+        ans[now_v] = "Yes";
+    }else{
+        auto iter = path_number.find(number[now_v]);
+        if(iter == path_number.end()){
+            path_number.insert(number[now_v]);
+            ans[now_v] = "No";
+        } else {
+            is_duplicated = true;
+            ans[now_v] = "Yes";
+        }
+    }
 
     for(auto next_v:g[now_v]){
         if(!is_visited[next_v]){
-            dfs(next_v, g, is_visited, path);
+            dfs(next_v, g, is_visited, number, path, path_number, is_duplicated, ans);
         }
     }
 
     path.pop_back();
+    path_number.erase(number[now_v]);
 
     return 0;
 
 }
-
 
 int main(){
     int n;
@@ -38,13 +49,19 @@ int main(){
         g[v].push_back(u);
     }
 
-    for(int i=0; i<n; i++) sort(g[i].begin(), g[i].end());
+    //for(int i=0; i<n; i++) sort(g[i].begin(), g[i].end());
 
     int now_v;
     vector<bool> is_visited(n, false);
     vector<int> path;
+    set<int> path_number;
+    bool is_duplicated = false;
+    vector<string> ans(n);
     now_v = 0;
-    dfs(now_v, g, is_visited, path);
+    dfs(now_v, g, is_visited, number, path, path_number, is_duplicated, ans);
+
+    for(auto ansi:ans) cout << ansi << " ";
+    cout << endl;
 
     return 0;
 }
