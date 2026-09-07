25ec5c0ba86de1416d3853507a5c463063cebc7e
sthr630-arcturus <s_izumi630y@yahoo.co.jp>
2026-09-08 00:00:23 +0900

    ABC444 level_c passed test case

---
+++ ABC444/c.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+bool is_atcoder_rico(vector<int>&a, int l){
+    bool is_ok;
+    int length;    
+    is_ok = true;
+    length = a.size();
+
+    if(length%2 == 0){
+        for(int i=0; i<length; i++){
+            if(a[i]+a[(length-1)-i] == l){
+                continue;
+            }else{
+                is_ok = false;
+                break;
+            }
+        }
+    }else{
+        is_ok = false;
+    }
+
+    return is_ok;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i=0; i<n; i++) cin >> a[i];
+    sort(a.begin(), a.end());
+
+    int l;
+    vector<int> ans;
+
+    l = a[0]+a[n-1];
+    if(is_atcoder_rico(a, l)) ans.push_back(l);
+
+    l = a.back();
+    while(a.back() == l) a.pop_back();
+    if(a.size() == 0){
+        ans.push_back(l);
+    }else{
+        if(is_atcoder_rico(a, l)) ans.push_back(l);
+    }
+
+    for(auto ansi:ans) cout << ansi << " ";
+    cout << endl;
+
+    return 0;
+
+}
--- ABC448/d.cpp
+++ ABC448/d.cpp
@@ -1,38 +1,26 @@
 #include<iostream>
 #include<vector>
-#include<set>
 #include<algorithm>
 using namespace std;
-int dfs(int now_v, vector<vector<int>> &g, vector<bool> &is_visited, vector<int> &number, vector<int> &path, set<int> &path_number, bool is_duplicated, vector<string> &ans){
-    
+int dfs(int now_v, vector<vector<int>> &g, vector<bool> &is_visited, vector<int> &path){
     is_visited[now_v] = true;
     path.push_back(now_v);
 
-    if(is_duplicated){
-        ans[now_v] = "Yes";
-    }else{
-        auto iter = path_number.find(number[now_v]);
-        if(iter == path_number.end()){
-            path_number.insert(number[now_v]);
-            ans[now_v] = "No";
-        } else {
-            is_duplicated = true;
-            ans[now_v] = "Yes";
-        }
-    }
+    cout << "--path--" << endl;
+    for(auto v:path) cout << v+1 << ", ";
+    cout << endl;
 
     for(auto next_v:g[now_v]){
         if(!is_visited[next_v]){
-            dfs(next_v, g, is_visited, number, path, path_number, is_duplicated, ans);
+            dfs(next_v, g, is_visited, path);
         }
     }
 
     path.pop_back();
-    path_number.erase(number[now_v]);
-
     return 0;
 
 }
+
 
 int main(){
     int n;
@@ -49,19 +37,13 @@ int main(){
         g[v].push_back(u);
     }
 
-    //for(int i=0; i<n; i++) sort(g[i].begin(), g[i].end());
+    for(int i=0; i<n; i++) sort(g[i].begin(), g[i].end());
 
     int now_v;
     vector<bool> is_visited(n, false);
     vector<int> path;
-    set<int> path_number;
-    bool is_duplicated = false;
-    vector<string> ans(n);
     now_v = 0;
-    dfs(now_v, g, is_visited, number, path, path_number, is_duplicated, ans);
-
-    for(auto ansi:ans) cout << ansi << " ";
-    cout << endl;
+    dfs(now_v, g, is_visited, path);
 
     return 0;
 }
